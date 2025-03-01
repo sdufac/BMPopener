a2534a27c744fef6012d13cfdb61eaf466dd7bea
simon <simon.dufac@gmail.com>
2025-03-01 15:14:42 +0100

    Get size of the image to set the size of the window

--- src/main.c
+++ src/main.c
@@ -6,7 +6,9 @@
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
-bool isFileBMP(char* filename);
+bool isFileBMP(FILE* image);
+uint32_t getImageWidth(FILE* image);
+uint32_t getImageHeight(FILE* image);
 
 int main(int argc,char * argv[])
 {
@@ -14,10 +16,18 @@ int main(int argc,char * argv[])
 	//--------------------------------------------------------------------------------------
 	
 	char* fileName = argv[1];
-	bool isBMP = isFileBMP(fileName);
+	FILE *image = fopen(fileName,"r");
+	bool isBMP = isFileBMP(image);
+	int screenWidth;
+	int screenHeight;
 
-	const int screenWidth = 800;
-	const int screenHeight = 450;
+	if(isBMP){
+		screenWidth = getImageWidth(image);
+		screenHeight = getImageHeight(image);
+	}else{
+		screenWidth = 500;
+		screenHeight = 500;
+	}
 
 	InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
@@ -55,16 +65,13 @@ CloseWindow();        // Close window and OpenGL context
 return 0;
 }
 
-bool isFileBMP(char* filename){
+bool isFileBMP(FILE* image){
 
 	uint16_t sgntr = 0x4D42;
 	uint16_t extractedSgntr = 0;
 	char buffer[2];
-	
 
-	FILE* image = fopen(filename,"r");
 	fread(buffer,2,1,image);
-
 	memcpy(&extractedSgntr,buffer,2);
 	
 	if(sgntr == extractedSgntr){
@@ -72,4 +79,30 @@ bool isFileBMP(char* filename){
 	}else{
 		return false;
 	}
+}
+
+uint32_t getImageWidth(FILE* image){
+	char buffer[4];
+	
+	uint32_t width = 0;
+
+	fseek(image,18,SEEK_SET);
+	fread(buffer, 4, 1 ,image);
+	memcpy(&width,buffer,sizeof(buffer));
+
+	printf("Largeur de l'image %d\n",width);
+	return width;
+}
+
+uint32_t getImageHeight(FILE* image){
+	char buffer[4];
+	
+	uint32_t height = 0;
+
+	fseek(image,22,SEEK_SET);
+	fread(buffer, 4, 1 ,image);
+	memcpy(&height,buffer,sizeof(buffer));
+
+	printf("Hauteur de l'image %d\n",height);
+	return height;
 }
